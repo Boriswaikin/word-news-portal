433704879e4408839644ffaef19c14da25a93735
Boriswaikin <plmnboris@gmail.com>
2023-04-10 23:56:20 -0700

    update two buttons, bookmark and question and add menu item

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import useNews from "../hooks/useNews";
 import { Outlet, Link } from "react-router-dom";
 
+
 export default function Home() {
   const navigate = useNavigate();
   const { isAuthenticated, loginWithRedirect } = useAuth0();
@@ -13,6 +14,39 @@ export default function Home() {
   return (
     <div className="home">
       <h1>World News</h1>
+      <div className="header">
+        <nav className="menu">
+          <ul className="menu-list">
+            <li>
+            {isAuthenticated?(<Link to="/app/Profile">Profile</Link>):
+            (<Link to="/" onClick={loginWithRedirect}>Profile</Link>)}
+            </li>
+            <li>
+            {isAuthenticated?(<Link to="/app/Bookmark">Profile</Link>):
+            (<Link to="/" onClick={loginWithRedirect}>Bookmark</Link>)}
+            </li>
+            <li>
+              <Link to="/app/debugger">Auth Debugger</Link>
+            </li>
+            <li>
+              {!isAuthenticated?(<button
+                className="exit-button"
+                onClick={()=>console.log("Signin")}
+              >
+                SignIn
+              </button>):
+              (<button
+                className="exit-button"
+                onClick={()=>console.log("logout")}
+              >
+                LogOut
+              </button>)
+              }
+            </li>
+          </ul>
+        </nav>
+        {/* <div>Welcome 👋 {user.name} </div> */}
+      </div>
       {/* <div className="btn-class"> */}
       {/* <button className="btn-primary" onClick={{}}>
             Home
@@ -28,8 +62,16 @@ export default function Home() {
         <ul className="newsList">
         {news.map((item,index)=>{
           return (
-            <li key={index} className="todo-item">
+            <li key={index} className="news-item">
               <Link to={`/news/${index}`}>{item.title}</Link>
+              <div className="item-button">
+              <button className="item-subButton" onClick={loginWithRedirect}>
+                <box-icon class="bookmark-logo" name='star'></box-icon>
+              </button>
+              <button className="item-subButton" onClick={()=>console.log("Ask GPT")}>
+                <box-icon class="chatGPT-logo" name='question-mark'></box-icon>
+              </button>
+              </div>
               {/* <div className="itemName">{item.title}</div> */}
             </li>)})}
              </ul>}
