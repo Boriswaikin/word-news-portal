370e46c3c61addff285ce4a01a2bfc2739233dc5
Boriswaikin <plmnboris@gmail.com>
2023-04-17 12:08:16 -0700

    update the fetch news method to get everything

--- client/src/hooks/useNews.js
+++ client/src/hooks/useNews.js
@@ -4,15 +4,29 @@ import { useAuthToken } from "../AuthTokenContext";
 export function useNews() {
   const [news, setNews] = useState();
   const [tempNews, setTempNews] = useState();
-
+  const to_date = new Date().toISOString().slice(0, 10);
+  const today = new Date(to_date);
+  const yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+  const yyyy = yesterday.getFullYear().toString();
+  const mm = (yesterday.getMonth() + 1).toString().padStart(2, '0');
+  const dd = yesterday.getDate().toString().padStart(2, '0');
+  const from_date = yyyy + '-' + mm + '-' + dd;
 
   useEffect(() => {
     async function getNews() {
       
-      const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${process.env.REACT_APP_NEWS_ID}`);
+      // const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${process.env.REACT_APP_NEWS_ID}`);
+       const res = await fetch(
+        `https://newsapi.org/v2/everything?`+
+        `q=business`+
+        `&language=en`+
+        `&sortBy=popularity`+
+        `&from=${from_date}&to=${to_date}`+
+        `&apiKey=${process.env.REACT_APP_NEWS_ID}`);
       const data = await res.json();
-      setNews(data.articles);
-      setTempNews(data.articles);
+      setNews(data.articles.slice(0,21));
+      setTempNews(data.articles.slice(0,21));
     }
 
     getNews();
@@ -27,10 +41,12 @@ export function useHotNews() {
   useEffect(() => {
     async function getHotNews() {
       const res = await fetch(
-        `https://newsapi.org/v2/top-headlines/sources?country=us&apiKey=${process.env.REACT_APP_NEWS_ID}`
+        `https://newsapi.org/v2/top-headlines/?`+
+        `country=us`+
+        `&apiKey=${process.env.REACT_APP_NEWS_ID}`
       );
       const data = await res.json();
-      setHotNews(data.sources);
+      setHotNews(data.articles);
     }
 
     getHotNews();
