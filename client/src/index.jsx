5fba21cd95fbc6483f857811bca8a1f935f4e09a
Jerry Wei <zywei86@gmail.com>
2023-04-20 12:37:56 -0700

    clean up index

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -10,7 +10,6 @@ import NotFound from "./components/NotFound";
 import Home from "./components/Home";
 import VerifyUser from "./components/VerifyUser";
 import AuthDebugger from "./components/AuthDebugger";
-// Auth0 Context, exposes Auth0 methods and data
 import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
 import { AuthTokenProvider } from "./AuthTokenContext";
 import "./style/normalize.css"
@@ -47,20 +46,15 @@ const root = ReactDOMClient.createRoot(container);
 root.render(
   <React.StrictMode>
     <Auth0Provider
-      // configuration from environment variables
-      // domain and client ID of the Auth0 application
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-      // window.location.origin - origin URL of the current window (no path)
       authorizationParams={{
         redirect_uri: `${window.location.origin}/verify-user`,
-        // audience of the Auth0 API - our todoAPI
         audience: process.env.REACT_APP_AUTH0_AUDIENCE,
-        // generate a token with specific scopes, the react app can only perform the actions
         scope: requestedScopes.join(" "),
       }}
     >
-      <AuthTokenProvider>   {/* provide context(access token) to children */}
+      <AuthTokenProvider> 
         <BrowserRouter>
           <Routes>
             {/* pages that don't require authentication */}
