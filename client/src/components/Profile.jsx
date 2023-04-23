2181130129d12164b3d51a74f0f55d268f440346
Jerry Wei <zywei86@gmail.com>
2023-04-23 00:06:00 -0700

    add edit state

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -7,7 +7,8 @@ export default function Profile() {
   const { user } = useAuth0();
   const { accessToken } = useAuthToken();
 
-  const[ userInfo, setUserInfo ] = useState({});
+  const [ userInfo, setUserInfo ] = useState({});
+  const [ edit, setEdit ] = useState(false);
 
   useEffect(() => {
     async function getUserInfo() {
@@ -31,36 +32,41 @@ export default function Profile() {
 
   return (
     <div className="profile">
-      <div className="user_icon">
-        <img src={user.picture} className="icon" width="70" alt="profile avatar" />
+      <div className="profile_panel">
+        <div className="user_icon">
+          <img src={user.picture} className="icon" width="70" alt="profile avatar" />
+        </div>
       </div>
 
-      <div className="profile-fields">
-        <div>
-          <p>Username: {userInfo?.name}</p>
-        </div>
-        <div>
-          <p>First name: {userInfo?.firstName}</p>
-        </div>
-        <div>
-          <p>Last name: {userInfo?.lastName}</p>
-        </div>
-        <div>
-          <p>Birthday: {userInfo?.birthday}</p>
-        </div>
-        <div>
-          <p>Gender: {userInfo?.gender}</p>
-        </div>
-        <div>
-          <p>Email: {userInfo?.email}</p>
-        </div>
-        <div>
-          <p>Phone: {userInfo?.phone}</p>
-        </div>
-        <div>
-          <p>Address: {userInfo?.address}</p>
-        </div>
-      </div>
+      { !edit ? (
+        <div className="profile-fields">
+          <div>
+            <p>Username: {userInfo?.name}</p>
+          </div>
+          <div>
+            <p>Email: {userInfo?.email}</p>
+          </div>
+          <div>
+            <p>First name: {userInfo?.firstName}</p>
+          </div>
+          <div>
+            <p>Last name: {userInfo?.lastName}</p>
+          </div>
+          <div>
+            <p>Birthday: {userInfo?.birthday}</p>
+          </div>
+          <div>
+            <p>Gender: {userInfo?.gender}</p>
+          </div>
+          <div>
+            <p>Phone: {userInfo?.phone}</p>
+          </div>
+          <div>
+            <p>Address: {userInfo?.address}</p>
+          </div>
+        </div> 
+      ) : (<p>hello</p>)
+      }
     </div>
-  );
+  )
 }
