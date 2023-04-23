61aef33054b1bdb37147acccb29ce6edb7bb5f20
Jerry Wei <zywei86@gmail.com>
2023-04-22 23:53:38 -0700

    get user info and display in profile

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,10 +1,33 @@
 import "../style/profile.css";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useAuthToken } from "../AuthTokenContext";
 import { useState ,useEffect } from "react";
 
 export default function Profile() {
   const { user } = useAuth0();
+  const { accessToken } = useAuthToken();
+
   const[ userInfo, setUserInfo ] = useState({});
+
+  useEffect(() => {
+    async function getUserInfo() {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/profile`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setUserInfo(data);
+      }
+    }
+
+    if(accessToken){
+      getUserInfo();
+    }
+  }, [accessToken]);
 
   return (
     <div className="profile">
@@ -14,28 +37,28 @@ export default function Profile() {
 
       <div className="profile-fields">
         <div>
-          <p>Username: {user.name}</p>
+          <p>Username: {userInfo?.name}</p>
         </div>
         <div>
-          <p>First name: {user.firstName}</p>
+          <p>First name: {userInfo?.firstName}</p>
         </div>
         <div>
-          <p>Last name: {user.lastName}</p>
+          <p>Last name: {userInfo?.lastName}</p>
         </div>
         <div>
-          <p>Birthday: {user.birthday}</p>
+          <p>Birthday: {userInfo?.birthday}</p>
         </div>
         <div>
-          <p>Gender: {user.gender}</p>
+          <p>Gender: {userInfo?.gender}</p>
         </div>
         <div>
-          <p>Email: {user.email}</p>
+          <p>Email: {userInfo?.email}</p>
         </div>
         <div>
-          <p>Phone: {user.phone}</p>
+          <p>Phone: {userInfo?.phone}</p>
         </div>
         <div>
-          <p>Address: {user.address}</p>
+          <p>Address: {userInfo?.address}</p>
         </div>
       </div>
     </div>
